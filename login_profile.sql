617a8d6bbc85b06c79c1f47ec6b43d20fe487e73
Noah Halstead <nhalstead00@gmail.com>
2017-11-21 21:07:10 -0500

    Updated the Database Import File

--- login_profile.sql
+++ login_profile.sql
@@ -1,41 +1,42 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.0
--- https://www.phpmyadmin.net/
+-- version 4.0.4.2
+-- http://www.phpmyadmin.net
 --
--- Vært: 127.0.0.1
--- Genereringstid: 19. 11 2017 kl. 20:21:04
--- Serverversion: 10.1.26-MariaDB
--- PHP-version: 7.1.8
+-- Host: localhost
+-- Generation Time: Nov 22, 2017 at 02:06 AM
+-- Server version: 5.6.13
+-- PHP Version: 5.4.17
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `login_profile`
 --
+CREATE DATABASE IF NOT EXISTS `login_profile` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `login_profile`;
 
 -- --------------------------------------------------------
 
 --
--- Struktur-dump for tabellen `permissions`
+-- Table structure for table `permissions`
 --
 
-CREATE TABLE `permissions` (
-  `permissions_id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `permissions` (
+  `permissions_id` int(11) NOT NULL AUTO_INCREMENT,
   `permissions_name` varchar(50) NOT NULL,
-  `permissions_cname` varchar(50) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+  `permissions_cname` varchar(50) NOT NULL,
+  PRIMARY KEY (`permissions_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=25 ;
 
 --
--- Data dump for tabellen `permissions`
+-- Dumping data for table `permissions`
 --
 
 INSERT INTO `permissions` (`permissions_id`, `permissions_name`, `permissions_cname`) VALUES
@@ -67,158 +68,119 @@ INSERT INTO `permissions` (`permissions_id`, `permissions_name`, `permissions_cn
 -- --------------------------------------------------------
 
 --
--- Struktur-dump for tabellen `roles`
+-- Table structure for table `roles`
 --
 
-CREATE TABLE `roles` (
-  `role_id` int(11) NOT NULL,
-  `role_name` varchar(50) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+CREATE TABLE IF NOT EXISTS `roles` (
+  `role_id` int(11) NOT NULL AUTO_INCREMENT,
+  `role_name` varchar(50) NOT NULL,
+  PRIMARY KEY (`role_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
 
 --
--- Data dump for tabellen `roles`
+-- Dumping data for table `roles`
 --
 
 INSERT INTO `roles` (`role_id`, `role_name`) VALUES
-(1, 'admin'),
-(2, 'moderator'),
-(3, 'member'),
-(4, 'guest');
+(1, 'ADMIN'),
+(2, 'MODERATOR'),
+(3, 'MEMBER'),
+(4, 'GUEST');
 
 -- --------------------------------------------------------
 
 --
--- Struktur-dump for tabellen `roles_and_permissions`
+-- Table structure for table `roles_and_permissions`
 --
 
-CREATE TABLE `roles_and_permissions` (
-  `fk_role_id` int(11) NOT NULL,
-  `fk_permissions_id` int(11) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+CREATE TABLE IF NOT EXISTS `roles_and_permissions` (
+  `insertId` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'This is for the Specific Role',
+  `weight` int(1) unsigned NOT NULL COMMENT 'Weight of the Permission. The Higher the More Weight it has.',
+  `uid` int(11) NOT NULL COMMENT 'Linking to the User''s account',
+  `permission_id` int(11) NOT NULL COMMENT 'Linking to the Role and it''s permissions',
+  PRIMARY KEY (`insertId`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=46 ;
 
 --
--- Data dump for tabellen `roles_and_permissions`
+-- Dumping data for table `roles_and_permissions`
 --
 
-INSERT INTO `roles_and_permissions` (`fk_role_id`, `fk_permissions_id`) VALUES
-(1, 1),
-(1, 2),
-(1, 3),
-(1, 14),
-(1, 13),
-(1, 12),
-(1, 11),
-(1, 10),
-(1, 9),
-(1, 8),
-(1, 7),
-(1, 6),
-(1, 5),
-(1, 4),
-(1, 20),
-(1, 21),
-(1, 22),
-(2, 2),
-(2, 3),
-(2, 4),
-(2, 5),
-(2, 7),
-(2, 9),
-(2, 10),
-(2, 11),
-(2, 20),
-(2, 21),
-(2, 22),
-(3, 16),
-(3, 17),
-(3, 20),
-(3, 21),
-(3, 22),
-(4, 15),
-(4, 19),
-(1, 24),
-(4, 23),
-(3, 23),
-(2, 24),
-(3, 24),
-(2, 12),
-(2, 8),
-(3, 18),
-(3, 19),
-(4, 24);
+INSERT INTO `roles_and_permissions` (`insertId`, `weight`, `uid`, `permission_id`) VALUES
+(1, 0, 1, 1),
+(2, 10, 34, 1),
+(3, 0, 1, 3),
+(4, 0, 1, 14),
+(5, 0, 1, 13),
+(6, 0, 1, 12),
+(7, 0, 1, 11),
+(8, 0, 1, 10),
+(9, 0, 1, 9),
+(10, 0, 1, 8),
+(11, 0, 1, 7),
+(12, 0, 1, 6),
+(13, 0, 1, 5),
+(14, 0, 1, 4),
+(15, 0, 1, 20),
+(16, 0, 1, 21),
+(17, 0, 1, 22),
+(18, 0, 2, 2),
+(19, 2, 2, 3),
+(20, 0, 2, 4),
+(21, 0, 2, 5),
+(22, 0, 2, 7),
+(23, 0, 2, 9),
+(24, 0, 2, 10),
+(25, 0, 2, 11),
+(26, 9, 34, 2),
+(27, 0, 2, 21),
+(28, 0, 2, 22),
+(29, 0, 3, 16),
+(30, 0, 3, 17),
+(31, 0, 3, 20),
+(32, 0, 3, 21),
+(33, 0, 3, 22),
+(34, 0, 4, 15),
+(35, 0, 4, 19),
+(36, 0, 1, 24),
+(37, 0, 4, 23),
+(38, 0, 3, 23),
+(39, 0, 2, 24),
+(40, 0, 3, 24),
+(41, 0, 2, 12),
+(42, 0, 2, 8),
+(43, 0, 3, 18),
+(44, 0, 3, 19),
+(45, 0, 4, 24);
 
 -- --------------------------------------------------------
 
 --
--- Struktur-dump for tabellen `users`
+-- Table structure for table `users`
 --
 
-CREATE TABLE `users` (
-  `uid` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `users` (
+  `uid` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `uname` varchar(30) DEFAULT NULL,
   `upass` varchar(50) DEFAULT NULL,
   `uemail` varchar(70) DEFAULT NULL,
   `fname` varchar(100) DEFAULT NULL,
   `lname` varchar(60) NOT NULL,
-  `phone` int(8) NOT NULL,
   `address` varchar(100) NOT NULL,
+  `zipcode` int(6) unsigned NOT NULL DEFAULT '123456',
   `city` varchar(100) NOT NULL,
-  `zipcode` int(4) NOT NULL,
-  `fk_role_id` int(11) NOT NULL,
-  `fk_users_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `phone` bigint(8) unsigned NOT NULL,
+  PRIMARY KEY (`uid`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=38 ;
 
 --
--- Data dump for tabellen `users`
+-- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`uid`, `uname`, `upass`, `uemail`, `fname`, `lname`, `phone`, `address`, `city`, `zipcode`, `fk_role_id`, `fk_users_id`) VALUES
-(33, 'sam', 'f16bed56189e249fe4ca8ed10a1ecae60e8ceac0', 'sam@live.dk', 'sam', 'sam', 44, '', '', 0, 3, 0),
-(36, 'spar', 'f412a2010eb6625c85ebe505e051e18008501c5e', 'spar@live.dk', 'spar', 'spar', 0, '', '', 0, 1, 0),
-(37, 'hej', '7110eda4d09e062aa5e4a390b0a572ac0d2c0220', 'hej@live.dk', 'hej', 'hej', 0, '', '', 0, 3, 0),
-(38, NULL, NULL, 'hej@live.dk', 'hej', 'hej', 45, 'dfgdfg', 'fgfdg', 400, 0, 37);
-
---
--- Begrænsninger for dumpede tabeller
---
-
---
--- Indeks for tabel `permissions`
---
-ALTER TABLE `permissions`
-  ADD PRIMARY KEY (`permissions_id`);
-
---
--- Indeks for tabel `roles`
---
-ALTER TABLE `roles`
-  ADD PRIMARY KEY (`role_id`);
-
---
--- Indeks for tabel `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`uid`);
-
---
--- Brug ikke AUTO_INCREMENT for slettede tabeller
---
-
---
--- Tilføj AUTO_INCREMENT i tabel `permissions`
---
-ALTER TABLE `permissions`
-  MODIFY `permissions_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
---
--- Tilføj AUTO_INCREMENT i tabel `roles`
---
-ALTER TABLE `roles`
-  MODIFY `role_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
---
--- Tilføj AUTO_INCREMENT i tabel `users`
---
-ALTER TABLE `users`
-  MODIFY `uid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;COMMIT;
+INSERT INTO `users` (`uid`, `uname`, `upass`, `uemail`, `fname`, `lname`, `address`, `zipcode`, `city`, `phone`) VALUES
+(34, 'spar', 'f412a2010eb6625c85ebe505e051e18008501c5e', 'mailme@gmail.com', 'spar', 'spar', '123 FakeIt Road', 156, 'WWW', 10545898),
+(35, 'hej', '7110eda4d09e062aa5e4a390b0a572ac0d2c0220', 'hej@live.dk', 'hej', 'hej', '', 123456, '', 0),
+(36, 'test', '7110eda4d09e062aa5e4a390b0a572ac0d2c0220', 'test@live.dk', 'test', 'test', '', 123456, '', 0),
+(37, 'noob', '34bcdf98deb05825ee8f40bad4b5912df89b0b95', 'noob@two.o', 'Noob', 'Noob', '', 123456, '', 0);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
